547328341ae072827d0e4b8ee8122dc8cfdc3678
Lauraducky <laura-h-95@live.com.au>
2015-10-07 15:53:44 +1000

    First few scripts added. No functionality yet

---
+++ Assets/Scripts/ArrowManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowManager : MonoBehaviour {
+
+	public static ArrowManager instance;
+
+	[SerializeField] Object[] arrows;
+	[SerializeField] Transform[] startPos;
+	[SerializeField] float interval = 1;
+	float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad (this);
+		} else {
+			Destroy(this);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer > interval) {
+			spawnArrow('N');
+		}
+	}
+
+	public void spawnArrow(char direction){
+		if (direction == 'N') {
+			Instantiate (arrows [0], startPos [0].position, Quaternion.identity);
+		} else if (direction == 'E') {
+			Instantiate (arrows [1], startPos [1].position, Quaternion.identity);
+		} else if (direction == 'S') {
+			Instantiate (arrows [2], startPos [2].position, Quaternion.identity);
+		} else if (direction == 'W') {
+			Instantiate (arrows [3], startPos [3].position, Quaternion.identity);
+		}
+	}
+}
---
+++ Assets/Scripts/Graveyard.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Graveyard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+		if (coll.gameObject.tag == "Arrow") {
+			print ("Arrow destroyed");
+			Destroy(coll.gameObject);
+		}
+	}
+}
