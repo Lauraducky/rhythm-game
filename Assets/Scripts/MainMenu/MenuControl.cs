14125785d92de9ab0b75c8aa7826ba76ad30e0f0
Evelyne Berger-Uichanco <evelyne.rose@ymail.com>
2016-03-06 16:24:06 +1000

    Started to build the menu system, added new scene for that, and added test text files

---
+++ Assets/Scripts/MainMenu/MenuControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class MenuControl : MonoBehaviour {
+    FileInfo[] files;
+
+    // Use this for initialization
+    void Start () {
+        DirectoryInfo songs = new DirectoryInfo(Directory.GetCurrentDirectory() + @"\Assets\Songs");
+        files = songs.GetFiles("*.txt");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI() {
+        int songnum = 0;
+        foreach (FileInfo file in files) {
+            GUI.Button(new Rect(20, 40*songnum+20, Screen.width - 40, 30), Path.GetFileNameWithoutExtension(file.ToString()));
+            songnum++;
+        }
+    }
+}
